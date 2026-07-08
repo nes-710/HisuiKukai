a460ea576d4fa4b76df80acc582591fbeb386166
nes-710 <nes710.s@gmail.com>
2026-07-08 15:28:41 +0900

    v0.0.3 ホーム画面追加

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return render_template("index.html")
+
+@app.route("/create")
+def create():
+    return render_template("create.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
