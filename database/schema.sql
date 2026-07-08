8da7cd10d72b26f60ab67ef780d8568bad97d11d
nes-710 <nes710.s@gmail.com>
2026-07-08 16:46:49 +0900

    v0.1.0 投稿データベースを追加

--- database/schema.sql
+++ database/schema.sql
@@ -15,3 +15,30 @@ CREATE TABLE IF NOT EXISTS contests (
 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS participants (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    contest_id INTEGER NOT NULL,
+
+    pen_name TEXT NOT NULL,
+    pen_name_kana TEXT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(contest_id) REFERENCES contests(id)
+);
+
+CREATE TABLE IF NOT EXISTS submissions (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    participant_id INTEGER NOT NULL,
+
+    theme TEXT NOT NULL,
+
+    poem TEXT NOT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY(participant_id) REFERENCES participants(id)
+);
