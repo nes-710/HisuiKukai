dd0428c1b7872696cc418407ecdecbac1732ccf2
nes-710 <nes710.s@gmail.com>
2026-07-08 22:30:36 +0900

    v0.8.5 マイグレーション機能を改善

--- migrate.py
+++ migrate.py
@@ -1,6 +1,6 @@
 import sqlite3
 from database import DATABASE
-
+import os
 
 def column_exists(conn, table_name, column_name):
     columns = conn.execute(f"PRAGMA table_info({table_name})").fetchall()
@@ -32,6 +32,9 @@ def main():
 
     print("マイグレーションが完了しました。")
 
+if not os.path.exists(DATABASE):
+    print("データベースが存在しないため初期化します。")
+    import init_db
 
 if __name__ == "__main__":
     main()
